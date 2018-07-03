a43c54a47643764d3dd754102f85a94b86163d46
matias <matiasruizr@hotmail.es>
2018-07-02 21:40:10 -0400

    Migrado a postgre

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_01_201309) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clientes", force: :cascade do |t|
     t.string "cliente_id"
     t.string "rut"
