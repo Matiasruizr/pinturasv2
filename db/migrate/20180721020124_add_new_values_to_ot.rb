c22e1b92becb335011382b440c38b8754fb46987
Matias <matiasruizr@hotmail.es>
2019-01-17 15:58:02 -0400

    test in show pdf

--- db/migrate/20180721020124_add_new_values_to_ot.rb
+++
@@ -1,6 +0,0 @@
-class AddNewValuesToOt < ActiveRecord::Migration[5.2]
-  def change
-    add_column :ots, :materia_prima_1_kg, :integer
-    add_column :ots, :materia_prima_1_porcentaje, :float
-  end
-end
